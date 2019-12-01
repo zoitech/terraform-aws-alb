d26bb00eee88dc0a56bf6fafad2bfa0d065e28f3
ZOI\graham.geary <gg@zoi.de>
2019-12-01 15:10:21 +0100

    moved aw_instance data source to data.tf

--- data.tf
+++ data.tf
@@ -1,2 +1,7 @@
 data "aws_caller_identity" "current" {
 }
+
+data "aws_instance" "target_group_instances" {
+  count       = length(split(",", var.target_ids))
+  instance_id = element(split(",", var.target_ids), count.index)
+}
