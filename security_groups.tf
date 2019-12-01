c0239930598530477b635a2a0b1b9383ebcd0e1e
ZOI\graham.geary <gg@zoi.de>
2019-12-01 15:12:39 +0100

    #13 amended security groups

--- security_groups.tf
+++ security_groups.tf
@@ -6,31 +6,54 @@ resource "aws_security_group" "lb_group" {
   vpc_id      = var.vpc_id
 }
 
-data "aws_instance" "target_group_instances" {
-  count       = length(split(",", var.target_ids))
-  instance_id = element(split(",", var.target_ids), count.index)
-
-}
-
-resource "aws_security_group" "group_loadbalancer_in" {
-  name        = "tf-rule-alb-${var.prefix}${var.lb_name}-in-${var.lb_in_from_port}-${var.lb_in_to_port}"
-  description = "Allow the load balancer in on ports ${var.lb_in_from_port}-${var.lb_in_to_port}"
+#http
+resource "aws_security_group" "group_loadbalancer_in_http" {
+  count       = local.create_sg_http_in
+  name        = "tf-rule-alb-${var.prefix}${var.lb_name}-in-${lookup(element(var.http_target_group_parameters, 0), "port")}-${lookup(element(var.http_target_group_parameters, length(var.http_target_group_parameters)), "port")}"
+  description = "Allow the load balancer in on ports ${lookup(element(var.http_target_group_parameters, 0), "port")}-${lookup(element(var.http_target_group_parameters, length(var.http_target_group_parameters)), "port")}"
   vpc_id      = var.vpc_id
 }
 
-resource "aws_security_group_rule" "rule_loadbalancer_in" {
-  description              = "Allow the load balancer in on ports ${var.lb_in_from_port}-${var.lb_in_to_port}"
+resource "aws_security_group_rule" "rule_loadbalancer_in_http" {
+  count                    = local.create_sg_http_in
+  description              = "Allow the load balancer in on ports ${lookup(element(var.http_target_group_parameters, 0), "port")}-${length(var.http_target_group_parameters)}"
   type                     = "ingress"
-  from_port                = var.lb_in_from_port
-  to_port                  = var.lb_in_to_port
+  from_port                = lookup(element(var.http_target_group_parameters, 0), "port")
+  to_port                  = lookup(element(var.http_target_group_parameters, length(var.http_target_group_parameters)), "port")
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.lb_group.id
 
-  security_group_id = aws_security_group.group_loadbalancer_in.id
+  security_group_id = aws_security_group.group_loadbalancer_in_http[0].id
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  count                = length(split(",", var.target_ids))
-  security_group_id    = aws_security_group.group_loadbalancer_in.id
+resource "aws_network_interface_sg_attachment" "sg_http_attachment" {
+  count                = local.create_sg_http_attach
+  security_group_id    = aws_security_group.group_loadbalancer_in_http[0].id
+  network_interface_id = data.aws_instance.target_group_instances[count.index].network_interface_id
+}
+
+# https
+resource "aws_security_group" "group_loadbalancer_in_https" {
+  count       = local.create_sg_https_in
+  name        = "tf-rule-alb-${var.prefix}${var.lb_name}-in-${lookup(element(var.https_target_group_parameters, 0), "port")}-${lookup(element(var.https_target_group_parameters, length(var.https_target_group_parameters)), "port")}"
+  description = "Allow the load balancer in on ports ${lookup(element(var.https_target_group_parameters, 0), "port")}-${lookup(element(var.https_target_group_parameters, length(var.https_target_group_parameters)), "port")}"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_rule" "rule_loadbalancer_in_https" {
+  count                    = local.create_sg_https_in
+  description              = "Allow the load balancer in on ports ${lookup(element(var.https_target_group_parameters, 0), "port")}-${length(var.https_target_group_parameters)}"
+  type                     = "ingress"
+  from_port                = lookup(element(var.https_target_group_parameters, 0), "port")
+  to_port                  = lookup(element(var.https_target_group_parameters, length(var.https_target_group_parameters)), "port")
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_group.id
+
+  security_group_id = aws_security_group.group_loadbalancer_in_https[0].id
+}
+
+resource "aws_network_interface_sg_attachment" "sg_https_attachment" {
+  count                = local.create_sg_https_attach
+  security_group_id    = aws_security_group.group_loadbalancer_in_https[0].id
   network_interface_id = data.aws_instance.target_group_instances[count.index].network_interface_id
 }
